44846aa63cf82dfd0484c1d4b7052c64631878a0
mrozenva <mrozenva@gmail.com>
2023-05-14 19:53:46 -0400

    prompt change

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,21 +2,14 @@ import { OpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain, ChatVectorDBQAChain } from 'langchain/chains';
 
-const CONDENSE_PROMPT = `You are a repeating program, your only job is to repeat the question verbatim given to you. Below are examples:
+const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
-Question: What is my dogs name?
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:` ;
 
-Answer: What is my dogs name?
-
-Question: What day of the week is it?
-
-Answer: What day of the week is it?
-
-Input: {question}
-
-` ;
-
-const QA_PROMPT = `You are an expert financial analyst. You are qualified and adept at seeing 10-k reports and pulling key information out of them. Use the following pieces of context to answer the question at the end.
+const QA_PROMPT = `You are an expert financial analyst. You are qualified and adept at seeing Form 10-K financial reports and extracting and interpreting key financial information out of them. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
